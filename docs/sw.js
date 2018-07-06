ec74ea75a3452afd0f30db864d6b618ae7c027ad
Milton <miltoncandelero@gmail.com>
2018-07-06 15:08:47 -0300

    Added manifest

---
+++ docs/sw.js
@@ -0,0 +1,75 @@
+//This is the service worker with the Cache-first network
+
+var CACHE = '0.1';
+var precacheFiles = [
+		//add all your files
+		'.\favicon.png',
+		'.\generationInfo.json',
+		'.\index.html',
+		'.\manifest.json',
+		'.\OfflinePWA.js',
+		'.\register.js',
+		'.\sw.js',
+		'.\img\floor.png',
+		'.\img\gameboy.png',
+		'.\img\green.png',
+		'.\lib\FileSaver.min.js',
+		'.\lib\howler.min.js',
+		'.\lib\manup.min.js',
+		'.\lib\pako.min.js',
+		'.\manifest\default.json',
+		'.\snd\receptors.mp3'
+    ];
+
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+  evt.waitUntil(precache().then(function() {
+    console.log('Skip waiting on install');
+    return self.skipWaiting();
+  }));
+});
+
+
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+  console.log('Claiming clients for current page');
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.'+ evt.request.url);
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
+});
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
+  });
+}
+
+function fromCache(request) {
+  //we pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  //this is where we call the server to get the newest version of the 
+  //file to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+function fromServer(request){
+  //this is the fallback if it is not in the cache to go to the server and get it
+  return fetch(request).then(function(response){ return response});
+}
